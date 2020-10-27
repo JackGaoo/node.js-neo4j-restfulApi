106d9751c4adeb3c65caebf74d7b5b6e2e0ee23a
jacque0754 <jacque0754@gmail.com>
2020-10-27 23:39:19 +0800

    count disease occurrance

--- app.js
+++ app.js
@@ -33,16 +33,31 @@ app.post('/', (req, res) => {
     .catch(error => res.status(500).send(error));
 });
 
-app.get('/search', (req, res) => {
-  console.log("get nodes");
+app.get('/search', async (req, res) => {
   const name = req.body.name;
-  db.get_one_Node(name)
-    .then((nodes) => {
-      res.json(nodes);
-      //res.render('./home.pug', { nodes });
-    })
-    .catch(error => res.status(404).send(error));
-});
+  let results = [];
+  for (let i = 0; i < name.length; i++) {
+    console.log("get one node");
+    await db.get_one_Node(name[i])
+      .then((nodes) => {
+        console.log(typeof(nodes));
+        results = results.concat(nodes);
+        //res.render('./home.pug', { nodes });
+      })
+      .catch(error => res.status(404).send(error));
+  }
+  // loop result array to count the occurrence of each disease
+  let disease_count = {};
+  for (let disease of results) {
+    const name = disease.name
+    if (disease_count.hasOwnProperty(name)) {
+      disease_count[name] += 1;
+    } else {
+      disease_count[name] = 1;
+    }
+  }
+  res.json(disease_count);
+})
 
 app.post('/clear', (req, res) => {
   db.clearNodes()
