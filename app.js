2f5f247f22f1b4f14e81d54f3a73b8c55e1500af
JackGaoo <49218708+JackGaoo@users.noreply.github.com>
2020-11-05 15:07:44 +1100

    add sort to the probabilities from big to small

--- app.js
+++ app.js
@@ -71,6 +71,16 @@ app.post('/Diagnosis', async (req, res) => {
                           .reduce(function(a, b){
                               return disease_count[a] > disease_count[b] ? a : b
                           });
+//sort the probabilities from big to small
+  let sort_Disease_count = {};
+  let sortDisease = Object.keys(disease_count)
+                          .sort(function (a, b){
+                              return disease_count[b] - disease_count[a];
+                          });
+for( let i = 0; i <= sortDisease.length; i++){
+  sort_Disease_count[sortDisease[i]] = disease_count[sortDisease[i]];
+}
+  console.log(sort_Disease_count);
   //console.log(disease_count[currDisease]);
   console.log(currDisease, disease_count[currDisease]);
   let currSymps = {};
@@ -95,7 +105,7 @@ app.post('/Diagnosis', async (req, res) => {
                            {"id": "absent",
                             "label": "No"}];
 
-  let finalReturn = {"question": question, "probability": disease_count};
+  let finalReturn = {"question": question, "probability": sort_Disease_count};
   res.json(finalReturn);
 })
 
